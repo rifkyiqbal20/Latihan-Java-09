98a5e1db5f20a02f2ad25b83e5e6572111e8c2d1
rifkyiqbal20 <rifkyiqbalpermadi20@gmail.com>
2019-11-23 08:11:57 +0700

    Latihan-Java-09

---
+++ 10.Logika/src/l10/logika/L10Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l10.logika;
+
+/**
+ * NAMA  : Muhamad Haidzir Ismail
+ * NIM   : A2.1900113
+ * Kelas : TI-1C
+ * 
+ * @author muhamadhaidzirismail
+ */
+public class L10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 23;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
---
+++ 15.Logika/src/l15/logika/L15Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l15.logika;
+
+/**
+ * NAMA  : Muhamad Haidzir Ismail
+ * NIM   : A2.1900113
+ * Kelas : TI-1C
+ * 
+ * @author muhamadhaidzirismail
+ */
+public class L15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       boolean a = true;
+       boolean b = true;
+       System.out.println("a && b = " + (a && b));
+       System.out.println("a || b = " + (a || b));
+       System.out.println("a !a = " +  !a);
+       System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
